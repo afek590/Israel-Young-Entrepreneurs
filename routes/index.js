e6a8fe2d0ba625b6ec99d48455463beea1f6cc2f
Afek H <afek590@gmail.com>
2017-03-11 11:50:31 +0200

    Commit before google account API changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'יזמים צעירים ישראל',
-  logo: '/images/logo.jpg'});
+  logo: '/images/logo.jpg',
+  address: 'כתובת מיקוד עיר'});
 });
 
 module.exports = router;
